eff294f5b4ebe6e5958995f6ff1f3f21e314b1ba
dingyanfeng <104774455+dingyanfeng@users.noreply.github.com>
2023-06-06 14:22:35 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
+#include"sudoku.h"
 
 using namespace std;
 
 int main(){
+    vector<vector<int>> matrix(9, vector<int>(9, -1));
+    SudoKu test(matrix);
+    test.create_random_sudoku(1, matrix);
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
