0583a2e5f1aef46e2eacd0c32199d7f1a4bf48a1
丁延峰 <3181660979@qq.com>
2023-06-08 15:55:13 +0800

    pre work

---
+++ sudoku-game/assert.h
@@ -0,0 +1,15 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#include<iostream>
+
+// ×Ô¶¨Òå¶ÏÑÔ
+void assert(bool cond,const char* message) {
+    if (!cond) {
+        std::cout << message << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+#endif
+
