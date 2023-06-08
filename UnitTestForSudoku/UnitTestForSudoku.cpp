824093002b12eec3410bf3e8c2ee292a8be99688
丁延峰 <3181660979@qq.com>
2023-06-08 23:08:06 +0800

    add unittest of solve_with_count

--- UnitTestForSudoku/UnitTestForSudoku.cpp
+++ UnitTestForSudoku/UnitTestForSudoku.cpp
@@ -3,7 +3,6 @@
 #include "../sudoku-game/common.h"
 #include "../sudoku-game/assert.h"
 #include "../sudoku-game/getopt.h"
-#include "../sudoku-game/sudoku.h"
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -13,9 +12,9 @@ namespace UnitTestForSudoku
 	TEST_CLASS(UnitTestForSudoku)
 	{
 	public:
-		
 		TEST_METHOD(TestMethod1)
 		{	
+			// ≤‚ ‘∫Ø ˝SudoKu::swap_col
 			SudoKu test1;
 			vector<vector<int>> matrix1 = {
 				{0, 2, 3, 4, 5, 6, 7, 9, 8},
@@ -40,12 +39,31 @@ namespace UnitTestForSudoku
 				{6, 2, 4, 9, 7, 8, 5, 1, 3}
 			};
 			test1.swap_col(1, 2, matrix1);
-
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
 					Assert::AreEqual(matrix1[i][j], matrix1_result[i][j]);
 				}
 			}
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			// ≤‚ ‘∫Ø ˝SudoKu::solve_with_count
+			SudoKu test2;
+			vector<vector<int>> matrix2 = {
+				{0, 6, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 5, 0, 0},
+				{2, 0, 0, 0, 0, 0, 0, 9, 0},
+				{0, 5, 3, 9, 0, 0, 1, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 7, 0},
+				{9, 0, 8, 0, 0, 4, 0, 5, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{7, 0, 0, 0, 0, 1, 0, 2, 0},
+				{0, 0, 0, 4, 0, 0, 7, 8, 9}
+			};
+			int num2 = 0;
+			Assert::AreEqual(2, test2.solve_with_count(matrix2, num2));
+			Assert::AreEqual(2, num2);
 		}
 	};
 }
